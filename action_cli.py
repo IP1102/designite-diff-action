0ae3140e3c9f78b467055732c10b68a0e3b7f511
Indranil <indranil1102@gmail.com>
2023-11-19 02:56:02 -0400

    modified code

--- action_cli.py
+++ action_cli.py
@@ -14,15 +14,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--token", dest="token", help="API token")
 parser.add_argument("--username", dest="user", help="User name")
 parser.add_argument("--repo-name", dest="repo", help="Repo name")
-parser.add_argument("--artifact-id", dest="artifact", help="Artifact ID")
 
-def download_artifact(token, artifact_id, user, repo):
+def download_artifact(token, artifact_id, repo):
     headers = {"Authorization": f"Bearer {token}"}
     timeout = 10  # Set the timeout value in seconds
-    r = requests.get(f"https://api.github.com/repos/{user}/{repo}/actions/artifacts/{artifact_id}", headers=headers, timeout=timeout)
+    r = requests.get(f"https://api.github.com/repos/{repo}/actions/artifacts/{artifact_id}", headers=headers, timeout=timeout)
     print(r.json())
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    download_artifact(args.token, args.artifact, args.user, args.repo)
+    download_artifact(args.token, args.artifact, args.repo)
     
