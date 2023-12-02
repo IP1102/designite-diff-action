b27370e85bed35bbcb61918ffa6f100f54cb9e87
Indranil <indranil1102@gmail.com>
2023-12-02 17:03:28 -0400

    print headers

--- action_cli.py
+++ action_cli.py
@@ -36,7 +36,7 @@ def validate(token, designite_output, repo):
             artifact_id = artifacts["id"]
             print(artifact_id)
             r = requests.get(artifacts["archive_download_url"], headers=headers, timeout=timeout)
-            print(r)
+            print(r.headers)
             print("Extracting")
             print(os.listdir())
             break
